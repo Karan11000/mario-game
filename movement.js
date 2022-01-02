25361f37637f16a6838707694d26a8c2a165da0f
Karan11000 <76610086+Karan11000@users.noreply.github.com>
2022-01-02 20:28:59 +0530

    Create movement.js

---
+++ movement.js
@@ -0,0 +1,10 @@
+const movement = {
+    update(gameObj) {
+        gameObj.entities.goombas.forEach((goomba) => {
+            goomba.currentState.movement(gameObj);
+        });
+        gameObj.entities.koopas.forEach((koopa) => {
+            koopa.currentState.movement(gameObj);
+        })
+    }
+}
