e011a66ec9e96e041b96056505aaa3f4fc716b36
Karan11000 <76610086+Karan11000@users.noreply.github.com>
2022-01-02 20:26:21 +0530

    Create animation.js

---
+++ animation.js
@@ -0,0 +1,12 @@
+let animation={
+    update(gameObj){
+let mario=gameObj.entities.mario;
+mario.currentState(gameObj);
+gameObj.entities.goombas.forEach((goomba)=>{
+    goomba.currentState.animation(gameObj);
+});
+gameObj.entities.koopas.forEach((koopa)=>{
+    koopa.currentState.animation(gameObj);
+});
+    }
+}
